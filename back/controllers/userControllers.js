7b6fa529f68b6b39c64eb67765d063fc10adc233
JB-Dev <jibaoke@gmail.com>
2022-09-02 11:53:46 +0200

    validation password with check password

--- back/controllers/userControllers.js
+++ back/controllers/userControllers.js
@@ -1,6 +1,6 @@
 import UserModel from '../models/userModel.js';
 import bcrypt from 'bcrypt';
-
+import passwordSchema from '../models/passwordModels.js';
 import ObjectID from 'mongoose';
 
 // Get all users
@@ -39,27 +39,38 @@ export const getUser = async (req, res) => {
 
 //updateUser
 export const updateUser = async (req, res) => {
+    
   if (!ObjectID.isValidObjectId(req.params.id))
     return res.status(400).send('ID unknow:' + req.params.id);
   const id = req.params.id;
   const { currentUserId, currentUserAdminStatus, password } = req.body;
 
-  if (id === currentUserId || currentUserAdminStatus) {
-    try {
-      if (password) {
-        const salt = await bcrypt.genSalt(10);
-        req.body.password = await bcrypt.hash(password, salt);
-      }
-      const user = await UserModel.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
+  if (!passwordSchema.validate(req.body.password)) {
+    res.status(400).json({
+      message:
+        'Le Mot de passe doit faire 10 caractère au moins, comprenant une majuscule, une mininuscule et un moins un chiffre.',
+    });
+  }else {
 
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+      if (id === currentUserId || currentUserAdminStatus) {
+        try {
+          if (password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(password, salt);
+          }
+          const user = await UserModel.findByIdAndUpdate(id, req.body, {
+            new: true,
+          });
+    
+          res.status(200).json(user);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+      } else {
+        res
+          .status(403)
+          .json('Accés interdit, mettez simplement VOTRE profil a jour');
+      }
   }
-  else{
-    res.status(403).json("Accés interdit, mettez simplement VOTRE profil a jour")
-  }
+
 };
--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { passwordCheck } from '../middleware/passwordCheck.js';
 import { getAllUsers, getUser, updateUser } from '../controllers/userControllers.js';
 
 const router = express.Router();
@@ -7,15 +6,8 @@ const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
-router.put('/:id', passwordCheck, updateUser);
+router.put('/:id', updateUser);
 
 export default router;
 
 
-/*
-    "livesIn":"Paris",
-    "worksAt":"Comptable",
-    "relationship":"celibataire",
-    "country":"France"
-    
-    */
