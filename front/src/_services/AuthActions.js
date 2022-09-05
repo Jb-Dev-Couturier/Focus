b715a9eca92586894cb131df12a73ff8e842e414
JB-Dev <jibaoke@gmail.com>
2022-09-05 23:11:12 +0200

    update css media query

--- front/src/_services/AuthActions.js
+++ front/src/_services/AuthActions.js
@@ -10,7 +10,6 @@ export const logIn = (formData, navigate) => async (dispatch) => {
     if(data.userAdmin){
       accountServices.saveUserAdmin(data.userAdmin);
     }
-    console.log(data);
   } catch (error) {
     console.log(error);
     dispatch({ type: "AUTH_FAIL" });
