28c3454f9b87f22f486b36adb8eae40334609e1b
JB-Dev <jibaoke@gmail.com>
2022-09-07 07:15:59 +0200

    update dashboard

--- front/src/components/Admin/CardDashboard/CardDashboard.jsx
+++ front/src/components/Admin/CardDashboard/CardDashboard.jsx
@@ -8,19 +8,20 @@ const CardDashboard = () => {
     //const usersData = useSelector((state) => state.usersReducer);
   return <div className="CardDash">
     {CardsDashData.map((card,id)=>{
-        return(
-            <div className="parentContainer">
-                <CardData
-                title={card.title}
-                color={card.color}
-                barValue={card.barValue}
-                value={card.value}
-                png={card.png}
-                series={card.series}
-                key={id}    
-                />
-            </div>
-        )
+        return (
+          <div className="parentContainer">
+            
+            <CardData
+              title={card.title}
+              color={card.color}
+              barValue={card.barValue}
+              value={card.value}
+              png={card.png}
+              series={card.series}
+              key={card.title}
+            />
+          </div>
+        );
     })}
   </div>;
 }
